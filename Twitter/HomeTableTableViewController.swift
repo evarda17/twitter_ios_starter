b02f71f062f51474c7d3bf80b6e23354fc2f509e
Eliza Vardanyan <evarda17@gmail.com>
2020-11-06 19:33:14 -0500

    Committing logout button creation

---
+++ Twitter/HomeTableTableViewController.swift
@@ -0,0 +1,41 @@
+//
+//  HomeTableTableViewController.swift
+//  Twitter
+//
+//  Created by Eliza Vardanyan on 11/6/20.
+//  Copyright © 2020 Dan. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    @IBAction func onLogout(_ sender: Any) {
+        TwitterAPICaller.client?.logout()
+        
+        
+    }
+    
+    
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+   
+
+}
